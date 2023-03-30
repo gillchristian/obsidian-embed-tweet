c517c273b3e2428634f61ed4740bc6ee9a47b29b
gillchristian <gillchristiang@gmail.com>
2023-03-30 10:36:29 +0200

    Add cache and fix CORS issue

--- main.ts
+++ main.ts
@@ -1,137 +1,96 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Plugin} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+type Cache = Record<string, Result>;
 
-interface MyPluginSettings {
-	mySetting: string;
+export default class TweetEmbedPlugin extends Plugin {
+  loadCache(): Promise<Cache> {
+    return this.loadData()
+      .then((data) => data ?? {})
+      .catch(() => ({}));
+  }
+
+  async onload() {
+    this.registerTweetProcessor();
+
+    this.addCommand({
+      id: 'reset-tweet-cache',
+      name: 'Reset cache',
+      callback: () => {
+        this.saveData({});
+      },
+    });
+  }
+
+  registerTweetProcessor() {
+    this.registerMarkdownCodeBlockProcessor('tweet', async (source, el, _ctx) => {
+      const cache = await this.loadCache();
+
+      const rows = source
+        .split('\n')
+        .map((r) => r.trim())
+        .filter((row) => row.length > 0);
+
+      const updatedCache = await rows.reduce(
+        (acc, tweet) => acc.then((cache) => insertTweetOnCacheIfMissing(cache, tweet)),
+        Promise.resolve(cache),
+      );
+
+      this.saveData(updatedCache);
+
+      rows.forEach((tweetUrl) => {
+        const tweet = updatedCache[tweetUrl];
+
+        if (!tweet) {
+          return;
+        }
+
+        const div = el.createEl('div');
+
+        if (tweet.success) {
+          div.innerHTML = tweet.html;
+        } else {
+          console.error(`Failed to fetch tweet: ${tweet.url}, Reason: ${tweet.error}`);
+          if (tweet.data) {
+            console.error(tweet.data);
+          }
+
+          div.innerHTML = `<pre>Failed to fetch tweet: ${tweet.url}</pre>`;
+        }
+      });
+    });
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const insertTweetOnCacheIfMissing = (cache: Cache, tweet: string): Promise<Cache> =>
+  cache[tweet] && cache[tweet].success
+    ? Promise.resolve(cache)
+    : fetchTweet(tweet).then((result) => ({...cache, [tweet]: result}));
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+type FailedResult = {error: string; url: string; data?: unknown; success: false};
+type SuccessResult = {html: string; success: true};
 
-	async onload() {
-		await this.loadSettings();
+type Result = SuccessResult | FailedResult;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+const fetchTweet = async (tweet: string): Promise<Result> => {
+  try {
+    const url = `https://publish.twitter.com/oembed?url=${encodeURIComponent(tweet)}&hide_thread=false`;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    const res = await fetch('https://cors-henna-seven.vercel.app/api/cors', {
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({url}),
+      method: 'POST',
+    });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    if (!res.ok) {
+      return {error: 'Failed to fetch', url: tweet, success: false};
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    const data = await res.json();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+    return typeof data.html === 'string'
+      ? {html: data.html, success: true}
+      : {error: 'Invalid response', url: tweet, data, success: false};
+  } catch (e) {
+    return {error: 'Failed to fetch', url: tweet, success: false};
+  }
+};
